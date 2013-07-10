2416ee575fd094de678ee14e30eefac0aeadd439
Dansanchez <daniel.sanbre@gmail.com>
2013-07-10 10:31:46 -0600

    First scenario (Start game) passed

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,12 +1,17 @@
 
 module Codebreaker
+
   class Game
    
     def initialize (output)
+      @output = output 
     end
 
     def start
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
     end
 
   end
+
 end
