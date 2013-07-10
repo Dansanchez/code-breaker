5f95aee70a107757209c1dea959f6138469e5159
Dansanchez <daniel.sanbre@gmail.com>
2013-07-10 11:30:58 -0600

    Working on the second feature (submits guess)

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -1,19 +1,3 @@
-
-Given /^I am not yet playing$/ do
-  
-end
-
-
-When /^I start a new game$/ do
-  game = Codebreaker::Game.new(output)
-  game.start
-end
-
-
-Then /^I should see "([^"]*)"$/ do |message|
-  output.messages.should include (message)
-end
-
 class Output
   def messages
     @messages ||= []
@@ -27,3 +11,30 @@ end
 def output
   @output ||= Output.new
 end
+
+Given /^I am not yet playing$/ do
+end
+
+Given /^the secret code is "([^"]*)"$/ do |secret|
+  @game = Codebreaker::Game.new(output)
+  @game.start(secret)
+end
+
+When /^I start a new game$/ do
+  game = Codebreaker::Game.new(output)
+  game.start('1234')
+end
+
+When /^I guess "([^"]*)"$/ do |guess|
+  @game.guess(guess)
+end
+
+Then /^I should see "([^"]*)"$/ do |message|
+  output.messages.should include(message)
+end
+
+Then /^the mark should be "([^"]*)"$/ do |mark|
+  output.messages.should include(mark)
+end
+
+
